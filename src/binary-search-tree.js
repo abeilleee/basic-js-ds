b4d2c34d35609941c7b853dd02252a80f198e7e4
May <abeillee@bk.ru>
2024-12-18 16:22:30 +0300

    feat: add solution for binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,48 +1,120 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null; 
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.rootNode) {
+      this.rootNode = newNode;
+      return;
+    }
+    this.insertNode(this.rootNode, newNode);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (!node.left) {                               //если слева нет узла добавляю новый туда, если есть запускаю ф-ию рекурсивно
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (!node.right) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) !== null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  find(data) {
+    return this.searchNode(this.rootNode, data);
+  }
+
+  searchNode(node, data) {
+    if (!node) {
+      return null;
+    }
+    if (data < node.data) {
+      return this.searchNode(node.left, data);
+    } else if (data > node.data) {
+      return this.searchNode(node.right, data);
+    } else {
+      return node; 
+    }
+  }
+
+  remove(data) {
+    this.rootNode = this.removeNode(this.rootNode, data);
+  }
+
+  removeNode(node, data) {
+    if (!node) {
+      return null;
+    }
+    if (data < node.data) {
+      node.left = this.removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this.removeNode(node.right, data);
+      return node;                                          //найденный узел
+    } else {
+      if (!node.left && !node.right) {
+        return null;                                      //узел не имеющий children
+      }
+      if (!node.left) {
+        return node.right;                                //у узла только right child
+      }
+      if (!node.right) {                                  //у узла только left child
+        return node.left; 
+      }
+      node.data = this.minNode(node.right).data;            //если у узла two children нахожу минимальный справа
+      node.right = this.removeNode(node.right, node.data);
+      return node;
+    }
+  }
+
+  minNode(node) {
+    while (node && node.left) {
+      node = node.left;
+    }
+    return node;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.minNode(this.rootNode) ? this.minNode(this.rootNode).data : null;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.maxNode(this.rootNode) ? this.maxNode(this.rootNode).data : null;
+  }
+
+  maxNode(node) {
+    while (node && node.right) {
+      node = node.right;
+    }
+    return node;
   }
 }
+
 
 module.exports = {
   BinarySearchTree
